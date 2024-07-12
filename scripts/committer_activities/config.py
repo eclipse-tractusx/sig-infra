d2709ff5dec2986a9d21954b4926c1f5ec2b39b8
Angelika Wittek <angelika.wittek@konteno.de>
2024-07-12 13:47:19 +0200

    feat: statistics for committer and contributor activities

---
+++ scripts/committer_activities/config.py
@@ -0,0 +1,18 @@
+import logging
+
+# GitHub handle
+username= ""
+# GitHub Personal Access Token with scope: repo, read:org, read:user scoped
+token = ""
+
+# GitHub Org Name / ID
+organizationName = "eclipse-tractusx"
+organizationID = "O_kgDOBh1dlg"
+
+
+# Script: github_contributions:
+contrib_nr_of_months=6
+fname_github_contributions = "GitHub_contributions.csv"
+
+loglevel=logging.INFO
+
